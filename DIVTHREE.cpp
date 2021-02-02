12df79b7744d522933f23ebae2253c15808a31a7
rollexxx23 <68501026+rollexxx23@users.noreply.github.com>
2021-02-02 10:29:14 +0530

    Update DIVTHREE.cpp

--- DIVTHREE.cpp
+++ DIVTHREE.cpp
@@ -1,26 +1,41 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
+
+void divthree(long long k , long long d , long long sum){
+    long long num ;
+	num = sum/k ;
+	if(num>d){
+	    cout<<d<<endl ;
+	}else{
+	    cout<<num<<endl ;
+	}
+}
 
 int main() {
 	// your code goes here
-	long long test ; 
+	//Arin
+	
+	
+	long long test ;
 	cin>>test ;
-	while(test--) {
-	long long n,k,d ;
-	cin>> n>>k>>d ;
-	long long arr[n] ;
-	long long sum = 0 ;
-	for(int i=0 ; i<n ;i++){
-	    cin>>arr[i] ;
-	    sum = sum + arr[i] ;
+	
+	while(test--){
+	    long long n,k,d ;
+	    cin>> n>>k>>d ;
+	    vector <long long> vect(n) ;
+	    
+	    long long sum = 0 ;
+	    for(int i=0 ; i<n ;i++){
+	    int a ;     
+	    cin>>a ;
+	    vect.push_back(a) ;
+	    sum = sum + a ;
 	}
-	long long ans ;
-	ans = sum/k ;
-	if(ans>d){
-	    cout<<d<<endl ;
-	}else{
-	    cout<<ans<<endl ;
-	}
+	
+	    divthree(k,d,sum) ;
+	
+	
 	}
 	return 0;
 }
