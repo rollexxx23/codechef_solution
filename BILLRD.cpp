6ac109be5662d23e3efacbe1ebd096f4e9e1db03
rollexxx23 <68501026+rollexxx23@users.noreply.github.com>
2021-02-02 10:27:48 +0530

    Update BILLRD.cpp

--- BILLRD.cpp
+++ BILLRD.cpp
@@ -1,75 +1,81 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+void billrd(long long n , long long k , long long x , long long y ){
+        k=k%4 ;    // pattern is repeated after 4 collision
+        long long array[4][2] ;
+	    if(x==y) {
+	       cout<<n<<" "<<n<<endl ;   // it will enter the top-right hole
+	        
+	        
+	    }
+	    else if(x>y){       // it will touch the right side
+	       
+	       y=n+y-x;
+	       x=n;
+	       
+	       array[1][0]=x;
+	       array[1][1]=y;
+	       
+	       x=y+x-n;
+	       y=n;
+	       
+	       array[2][0]=x;
+	       array[2][1]=y;
+
+           y=y-x;
+	       x=0;
+	       array[3][0]=x;
+	       array[3][1]=y;
+	       x=x+y;
+	       y=0;
+	       array[0][0]=x;
+	       array[0][1]=y;
+	       cout<<array[k][0]<<" "<<array[k][1]<<endl ;
+	        
+	        
+	    } else {      // it will touch the top 
+	        x=n-y+x;
+	        y=n;
+	        array[1][0]=x;
+	        array[1][1]=y;
+	        y=y+x-n;
+	        x=n;
+	        array[2][0]=x;
+	        array[2][1]=y;
+	        x=x-y;
+	        y=0;
+	        array[3][0]=x;
+	        array[3][1]=y;
+	       
+	        y=x+y;
+	        x=0 ;
+	        array[0][0]=x;
+	        array[0][1]=y;
+	        cout<<array[k][0]<<" "<<array[k][1]<<endl ;
+	        
+	    }
+	    
+	    
+	    
+	    
+}
+
 int main() {
 	// your code goes here
+	//Arin 
+	
 	int test ;
 	
 	cin>>test ;
 	while(test--) {
 	    long long n,k,x,y ;
 	    cin>>n>>k>>x>>y ;
-	      
-	    vector<vector<int>> co_ord( 4 , vector<int> (2, 0));
-	    k=k%4 ; //the path will be repeated after 4 collisions
-	    if(x==y) {
-	        cout<<n<<" "<<n<<endl ;
-	        continue ;
-	        
-	    }
-	    else if(x>y){
-	       
-	       y=n+y-x;
-	       x=n;
-	       
-	       co_ord[1][0]=x;
-	       co_ord[1][1]=y;
-	       
-	       x=y+x-n;
-	       y=n;
-	       
-	       co_ord[2][0]=x;
-	       co_ord[2][1]=y;
-
-           y=y-x;
-	       x=0;
-	       co_ord[3][0]=x;
-	       co_ord[3][1]=y;
-	       x=x+y;
-	       y=0;
-	       co_ord[0][0]=x;
-	       co_ord[0][1]=y;
-	        
-	        
-	    } else {
-	        x=n-y+x;
-	        y=n;
-	        co_ord[1][0]=x;
-	        co_ord[1][1]=y;
-	        y=y+x-n;
-	        x=n;
-	        co_ord[2][0]=x;
-	        co_ord[2][1]=y;
-	        x=x-y;
-	        y=0;
-	        co_ord[3][0]=x;
-	        co_ord[3][1]=y;
-	       
-	        y=x+y;
-	        x=0 ;
-	        co_ord[0][0]=x;
-	        co_ord[0][1]=y;
-	        
-	        
-	    }
-	    
-	    
-	    
-	    cout<<co_ord[k][0]<<" "<<co_ord[k][1]<<endl ;
-	    
-	    
+	    billrd(n,k,x,y) ;
 	    
 	    
 	}
 	return 0;
 }
+
